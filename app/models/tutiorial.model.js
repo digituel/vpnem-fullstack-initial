cd2f76cd0a4c033edbf91b4a9025ff5044a84533
digituel <100965556+digituel@users.noreply.github.com>
2024-08-08 14:16:41 -0400

    changes to server.js

--- app/models/tutiorial.model.js
+++ app/models/tutiorial.model.js
@@ -14,16 +14,6 @@ module.exports = mongoose => {
   return Tutorial;
 };
 
-{
-  "_id": "5e363b135036a835ac1a7da8",
-  "title": "Js Tut#",
-  "description": "Description for Tut#",
-  "published": true,
-  "createdAt": "2020-02-02T02:59:31.198Z",
-  "updatedAt": "2020-02-02T02:59:31.198Z",
-  "__v": 0
-}
-
 
 module.exports = mongoose => {
   var schema = mongoose.Schema(
@@ -44,13 +34,3 @@ module.exports = mongoose => {
   const Tutorial = mongoose.model("tutorial", schema);
   return Tutorial;
 };
-
-
-{
-  "title": "Js Tut#",
-  "description": "Description for Tut#",
-  "published": true,
-  "createdAt": "2020-02-02T02:59:31.198Z",
-  "updatedAt": "2020-02-02T02:59:31.198Z",
-  "id": "5e363b135036a835ac1a7da8"
-}
